28db137e8118d3ea509eb24370a25b788bd0f945
webintechteam@gmail.com <webintechteam@gmail.com>
2025-05-25 18:15:28 +0530

    improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import '../styles/Header.css';
 import { FiSearch, FiBell, FiPlus } from 'react-icons/fi';
 import logo from "../assets/logo.jpg";
-import avatar from "../assets/avatar.jpg"
+import avatar from "../assets/doctor.png"
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
         <img src={logo} alt="Healthcare Logo" className="logo" />
-        <h1>Healthcare.</h1>
+        <h1 style={{color:"#4287f5"}}>Health<span style={{color:"#1a093b"}}>care.</span></h1>
       </div>
       <div className="header-center">
         <div className="search-bar">
@@ -17,16 +18,17 @@ const Header = () => {
         </div>
       </div>
       <div className="header-right">
-        <button className="add-button">
-          <FiPlus />
+        <button className="create-appointment-button">
+          <FiPlus className="plus-icon" />
+          <span> Create Appointment</span>
         </button>
         <div className="notification">
           <FiBell />
           <span className="notification-badge">3</span>
         </div>
         <div className="user-profile">
-          <img src={avatar} alt="User" className="logo" />
-          <span>Dr. John Doe</span>
+          <img src={avatar} alt="User" className="avatar" />
+          <span>Dr. Suresh Ellur</span>
         </div>
       </div>
     </header>
