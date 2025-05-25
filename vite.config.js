358a04a011ef4de6541d1b74f407021f90d6965f
webintechteam@gmail.com <webintechteam@gmail.com>
2025-05-25 13:37:14 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH ||"/HealthCare/tree/master",
   optimizeDeps: {
     include: ['@react-three/fiber', '@react-three/drei'],
   },
